d44b7b6146dba09a211423aa45c56fdf302e3cbd
Mai Campbell <jirallc@gmail.com>
2020-02-03 15:17:58 -0800

    feat: add active class after checking if the element is near the top of the viewport

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+Create dynamic navbar menu using pure vanilla JavaScript.
+Add active state to the section, it should be clear which section is being viewed while scrolling 
+through the page.
+Check if the element is near the top of viewport if it is than add active class to the section.
+Smooth Scroll while clicking on the anchor tag and it should take user to that section.
--- js/app.js
+++ js/app.js
@@ -1,118 +1,100 @@
-
-
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Define Global Variables
- * 
-*/
+ *
+ */
 
+let sections = document.querySelectorAll("section");
+const navbar = document.getElementById("navbar__list");
 
-let sections = document.querySelectorAll('section')
-const navbar = document.getElementById('navbar__list');
-const landingSection = document.querySelectorAll('landing__container');
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
+// Create Dynamic navbar menu and add class to menu link to anchor tag.
 function createNavbar() {
+  for (let i = 0; i < sections.length; i++) {
+    const section = sections[i];
+    const liList = document.createElement("li");
+    let sectionLink = document.createElement("A");
 
-    for(let i = 0; i < sections.length; i++) {
-        const section = sections[i];
-        const liList = document.createElement('li');
-        let sectionLink = document.createElement('A');
+    liList.setAttribute("id", section.dataset.nav);
+    sectionLink.setAttribute("href", `#${sections[i].id}`);
+    sectionLink.textContent = `${sections[i].id}`;
+    sectionLink.classList.add("menu__link");
 
-        liList.setAttribute('id', section.dataset.nav)
-        sectionLink.setAttribute('href', `#${sections[i].id}`);
-        sectionLink.textContent = `${sections[i].id}`;
-        sectionLink.classList.add('menu__link');
+    liList.classList.add("links");
 
-        liList.classList.add('links');
-
-        liList.appendChild(sectionLink);
-        navbar.appendChild(liList);
-    }
-      
+    liList.appendChild(sectionLink);
+    navbar.appendChild(liList);
+  }
 }
 createNavbar();
 
+// Add class 'active' to the section when near top of viewport
+let landingSection = document.querySelector("div");
+let bounding = landingSection.getBoundingClientRect();
 
+let isInViewport = function(elem) {
+  let bounding = elem.getBoundingClientRect();
+  return (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    bounding.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
 
-// Add class 'active' to section when near top of viewport
-
+window.addEventListener("scroll", function(event) {
+    if (isInViewport(landingSection)) {
+      landingSection.classList.add("active_class");
+    }
+  }, false);
 
 // Scroll to anchor ID using scrollTO event
-let anchorLinks = document.querySelectorAll('a[href^="#"]')
- 
-for (let item of anchorLinks) { // relitere 
-    item.addEventListener('click', (e)=> {
-        let hashval = item.getAttribute('href')
-        let target = document.querySelector(hashval)
-        target.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        })
-        history.pushState(null, null, hashval)
-        e.preventDefault()
-    })
+let anchorLinks = document.querySelectorAll('a[href^="#"]');
+
+for (let item of anchorLinks) {
+  // relitere
+  item.addEventListener("click", e => {
+    let hashval = item.getAttribute("href");
+    let target = document.querySelector(hashval);
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+    history.pushState(null, null, hashval);
+    e.preventDefault();
+  });
 }
 
-
-
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
+// Set sections as active while scrolling
 
 //Found this solution from https://stackoverflow.com/questions/52025615/vanilla-js-change-active-state-of-links-when-scrolling-refactoring
 
-const links = document.querySelectorAll('.links');
-// const sections = document.querySelectorAll('section');
+const links = document.querySelectorAll(".links");
 
 function changeLinkState() {
   let index = sections.length;
 
-  while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
-    links.forEach((link) => link.classList.remove('active'));
- 
-  links[index].classList.add('active');
+  while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+  links.forEach(link => link.classList.remove("active"));
+
+  links[index].classList.add("active");
 }
 
 changeLinkState();
 
-window.addEventListener('scroll', changeLinkState);
+window.addEventListener("scroll", changeLinkState);
